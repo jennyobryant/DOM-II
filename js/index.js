aeb6e70e356ffa3c6511a874f9dd3c24b4688ae3
jenny obryant <jennyobryant@protonmail.com>
2019-08-10 13:05:57 -0600

    second draft

--- js/index.js
+++ js/index.js
@@ -2,3 +2,7 @@
 document.getElementById("fun-bus").addEventListener("mouseover", e => {
     console.log("have the mouseover event!");
 });
+
+document.querySelector(".text-heading").addEventListener('keydown', e => {
+    console.log("will for keydown!")
+});
